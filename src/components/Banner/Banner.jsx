ad6366d3cbfe637eeb8e2b02ba3e3854e3948d29
SamsonNicoleta <nicoletasamson194@gmail.com>
2023-03-22 15:59:35 +0200

    create Banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../../../../../CoffeeShop-main/src/components/Banner/Banner.css";
+import {NavLink} from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Banner = ({link}) => {
+    return (
+        <section className="container">
+            <div className="container-content">
+                <h1 className="container-title">
+                    Everything You Love About Coffee
+                </h1>
+                <img src={"Beans-logo.svg"} alt="logo"/>
+                <h2 className="container-sub-title">We makes every day full of energy and taste</h2>
+                <h2 className="container-sub-title">Want to try our beans?</h2>
+                <NavLink to={link}>
+                    <button className="more-btn btn-pos">
+                        More
+                    </button>
+                </NavLink>
+            </div>
+        </section>
+    );
+};
+
+Banner.propTypes = {
+    link: PropTypes.string
+};
+
+export default Banner;
