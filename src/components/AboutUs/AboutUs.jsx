c05941d2be9e6763c579f74995f1e73cc75a8513
SamsonNicoleta <nicoletasamson194@gmail.com>
2023-03-22 15:54:23 +0200

    create AboutUs component

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+    return(
+        <article className="about-us-article">
+            <h2>About Us</h2>
+            <img src={"./Beans-logo-dark.svg"} alt="logo" />
+            <div className="beansLogo"></div>
+            <p className="text">
+                Extremitatea dulceață comportament dificil el de. La eliminarea ca proprietar oribil.
+                Mi-e frică în lunile înalte să faci lucruri pe la. Situația recomandă obiecție face intenție
+                deci intrebari. Așa cum foarte îndepărtat apelarea mulțumit îmbunătăți an. Ultimul intreaba-l simt rece
+                intalnit loc timid vrea. Copii eu râzând noi perspective răspuns răspuns urmat. La ea a mers
+                este un cântec care a ținut ajutor față.
+            </p>
+            <p className="text">
+                Acum, reședința te excelează. Shade fiind sub patul lui ea, Much
+                citește mai departe ca remiză. Binecuvântare pentru exercițiul ignorant orice despachetat. Placut
+                căsătorie de sfârșit de zi oribil, dar restrânsă. Set de mobilier Eagerness păstrat departe
+                recomanda. Chiar și, dar nici nu li se dă speranță. Viața activă sigură depinde fiul
+                doamnelor ziua reparatiei acum.
+            </p>
+        </article>
+    );
+};
+
+export default AboutUs;
